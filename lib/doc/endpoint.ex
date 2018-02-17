17663a4230af586c644e5df8dfe9f22eb6eec1a1
Ian Luites <IanLuites@users.noreply.github.com>
2018-02-17 15:39:15 -0500

    Add endpoint structure.

---
+++ lib/doc/endpoint.ex
@@ -0,0 +1,35 @@
+defmodule APIDoc.Doc.Endpoint do
+  @moduledoc ~S"""
+
+  """
+  alias APIDoc.Doc.{Param, Response}
+
+  @typedoc @moduledoc
+  @type t :: %__MODULE__{
+          method: :get | :post | :put | :patch | :delete,
+          path: [String.t() | atom],
+          id: String.t(),
+          summary: String.t(),
+          description: String.t() | nil,
+          parameters: [Param.t()],
+          responses: [Response.t()],
+          tags: [String.t()]
+        }
+
+  @enforce_keys [
+    :method,
+    :path,
+    :id,
+    :summary
+  ]
+  defstruct [
+    :method,
+    :path,
+    :id,
+    :summary,
+    description: nil,
+    parameters: [],
+    responses: [],
+    tags: []
+  ]
+end
