23a7be2e5464046c8ed55dec99811e67a3313a0a
Ian Luites <IanLuites@users.noreply.github.com>
2018-02-17 14:23:43 -0500

    Add APIDoc macro.

--- lib/api_doc.ex
+++ lib/api_doc.ex
@@ -2,4 +2,11 @@ defmodule APIDoc do
   @moduledoc ~S"""
   API documentation generator for Elixir.
   """
+
+  @doc false
+  defmacro __using__(opts \\ []) do
+    quote do
+      use APIDoc.APIDocumenter, unquote(opts)
+    end
+  end
 end
